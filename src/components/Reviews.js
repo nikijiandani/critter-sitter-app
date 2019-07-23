462139adadba21aef2ed354c8030b772a7f92ec7
poushitaguha <pguha.kolkata@gmail.com>
2019-07-22 20:35:45 -0400

    Add func components in Profile

---
+++ src/components/Reviews.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/profile.css';
+
+function Reviews(props) {
+  
+  return (
+    <div className="profile-review">
+        <h4>Reviews</h4>
+        <ul className="reviews">
+        {props.reviews.map((review, index) => {
+            return (
+                <div key={index} className="review-item">
+                    <h5>{review.first_name}</h5>
+                    <p>{review.content}</p>
+                </div>
+            )
+        })}
+        </ul>
+    </div>
+  )
+}
+
+export default Reviews;
