71c16caa508b543cd8fd6531325c3805c69f7638
poushitaguha <pguha.kolkata@gmail.com>
2019-07-24 14:16:56 -0400

    Display messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -5,46 +5,57 @@ class Messages extends Component {
   constructor() {
     super()
     this.state = {
-      messages: [
-        {
-          from_id: "Dudley",  //1
-          to_id: "Lester",    //2
-          content: "Hey, I'm looking for a sitter for New Year's eve this year. Are you available?",
-          created_at: "July 22, 2019 03:24:00"
-        },
-        {
-          from_id: "Lester",  //2
-          to_id: "Dudley",    //1
-          content: "Sure! I don't mind. Looks like you're in my area too!",
-          created_at: "July 22, 2019 05:24:00"
-        },
-        {
-          from_id: "Dudley",  //1
-          to_id: "Lester",    //2
-          content: "Great! Talk to you soon! :)",
-          created_at: "July 22, 2019 06:24:00"
-        },      
-      ]
+      data: null
     }    
   }
 
+  componentDidMount() {
+    fetch('http://localhost:8080/api/messages?user_id=1')
+    .then(results => {
+      results.json().then((res) => {
+        console.log(res)
+        this.setState({
+          data: res
+        });
+      })
+    })
+  }  
+
   render() {
+    if (!this.state.data) {
+      return (<div></div>)
+    }
     return (
       <div className="message-container">
-        <div className="message-block">
-          <h4>Conversation with Lester</h4>
+        <div className="message-block-to">
+          <h4>Messages To {this.state.data.first_name}</h4>
           <ul className="message-list">
-          {this.state.messages.map((message, index) => {
-              return (
-                  <div key={index} className="message-item">
-                      <h5>{message.from_id}:</h5>
-                      <p>{message.content}</p>
-                      <p>{message.created_at}</p>
-                  </div>
-              )
-          })}
+            <div className="message-item">
+                  {this.state.data.to_me.map((to, ind) =>
+                    <div key={ind}>
+                      <h5>{to.first_name} {to.last_name}:</h5>
+                      <p>{to.content}</p>
+                      <p>{new Date(to.created_at).toLocaleString()}</p>
+                    </div>   
+                  )}
+            </div>
           </ul>
         </div>
+
+        <div className="message-block-from">
+          <h4>Messages From {this.state.data.first_name}</h4>
+          <ul className="message-list">
+            <div className="message-item">
+                  {this.state.data.from_me.map((from, ind) =>
+                    <div key={ind}>
+                      <h5>{from.first_name} {from.last_name}:</h5>
+                      <p>{from.content}</p>
+                      <p>{new Date(from.created_at).toLocaleString()}</p>
+                    </div>   
+                  )}
+            </div>
+          </ul>
+        </div>        
       </div>
     )
   }
