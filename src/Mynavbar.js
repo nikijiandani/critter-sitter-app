e47e6ef1a8875953effdd987ed57444d6a48ec90
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-25 14:42:05 -0400

    Login/Logout with local storage

--- src/Mynavbar.js
+++ src/Mynavbar.js
@@ -1,34 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from './logo.svg';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Button} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
-const Mynavbar = () => {
-  return(
-    <Navbar className="nav-color" expand="lg">
-      <Navbar.Brand href="/">
-        <img
-        src={logo}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="logo"
-        />
-        {' critterSitter'}
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-        <Nav>
-          <Nav.Link href="/signup">Signup</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link className="message-icon" href="/messages">
-            <FontAwesomeIcon icon={faEnvelope} />
-          </Nav.Link>               
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
+class Mynavbar extends Component{
+
+  render () {
+    return(
+      <Navbar className="nav-color" expand="lg">
+        <Navbar.Brand href="/">
+          <img
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="logo"
+          />
+          {' critterSitter'}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            { this.props.currentUser ? (
+              <>
+                <Navbar.Text>
+                  Logged in as:{this.props.currentUser.email}
+                </Navbar.Text>
+                <Button onClick={this.props.handleLogout}>Logout</Button>
+                <Link className="message-icon" to="/messages">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </Link>
+              </> ) : (
+              <>
+                <Link to="/signup">Signup</Link>
+                <Link to="/login">Login</Link>
+              </>
+              )
+            }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
 }
 
 export default Mynavbar;
