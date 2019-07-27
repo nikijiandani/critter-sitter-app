e4fcb07a99a9829fb1aa1b5dd7b614c672505253
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-27 13:44:52 -0400

    Do not show logged in users profile

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -7,40 +7,46 @@ function ProfileList(props) {
   return (
     <ul className="profilelist">
       {props.profiles.map(user => {
-        return (
-        <li className="item" key={user.user_id}>
-          <img src={user.avatar} alt="avatar" className="avatar" />
-          <div className="item-details">
-            <Link to={`/profile/${user.user_id}`}>{user.first_name} {user.last_name}</Link>
-            <div className="pet">
-              <div className="pet-text">
-                { user.role === 2 ?
-                <p>Looks after</p>
-                :
-                <p>Owns</p>
-                }
+        console.log("This is localstrage", typeof localStorage.getItem('loggedInUsersId'))
+        console.log(typeof user.user_id)
+        if(user.user_id === parseInt(localStorage.getItem('loggedInUsersId'))){
+          return <li style={{display: "none"}} key={"do-not-show"}></li>
+        } else {
+          return (
+          <li className="item" key={user.user_id}>
+            <img src={user.avatar} alt="avatar" className="avatar" />
+            <div className="item-details">
+              <Link to={`/profile/${user.user_id}`}>{user.first_name} {user.last_name}</Link>
+              <div className="pet">
+                <div className="pet-text">
+                  { user.role === 2 ?
+                  <p>Looks after</p>
+                  :
+                  <p>Owns</p>
+                  }
+                </div>
+                <div className="pet-icon">
+                    {user.sitter_pet_types.map((pet) => 
+                    <div key={pet.pet_type_id}>
+                      {pet.icon}
+                    </div>
+                    )}
+                </div>
               </div>
-              <div className="pet-icon">
-                  {user.sitter_pet_types.map((pet) => 
-                  <div key={pet.pet_type_id}>
-                    {pet.icon}
-                  </div>
-                  )}
+              <div className="location">
+                {user.city}, ON, {user.postal_code}
+              </div>
+              <div className="rating">
+                <StarRatingComponent 
+                    name="rate1" 
+                    starCount={5}
+                    value={parseInt(user.avg_rating)}
+                />
               </div>
             </div>
-            <div className="location">
-              {user.city}, ON, {user.postal_code}
-            </div>
-            <div className="rating">
-              <StarRatingComponent 
-                  name="rate1" 
-                  starCount={5}
-                  value={parseInt(user.avg_rating)}
-              />
-            </div>
-          </div>
-        </li>
-        )
+          </li>
+          )
+        }
       })}
     </ul>
   )
