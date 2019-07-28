9fac3b5a42b44f9c76109ab1480c0bbc2b5fa0d9
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-27 20:58:58 -0400

    fix msgs component

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,58 +1,48 @@
 import React, { Component } from 'react';
+import {Form, Button} from 'react-bootstrap'
 import './styles/messages.css'
 
 class Messages extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: null,
-
-    }
-  }
-
-  componentDidMount() {
-
-    const currentUserId = localStorage.getItem('loggedInUsersId')
-
-    fetch(`http://localhost:8080/api/messages?user_id=${currentUserId}`)
-    .then(results => {
-      results.json().then((res) => {
-        console.log(res)
-        this.setState({
-          data: res
-        });
-      })
-    })
-  }  
-
   render() {
-    if (!this.state.data) {
-      return (<div></div>)
+    if (!this.props.msgs) {
+      return (<div>No messages found!</div>)
     }
+
+    const messages = this.props.msgs.map((msg, index) => {
+      if(msg.other_user_id === this.props.currentProfile){
+        if(msg.msg_dir === 'fromuser'){
+          return (<span className="message-item-fromuser" key={index}>
+            <h5>Me:</h5>
+            <p>{msg.content}</p>
+            <p className="text-muted">{new Date(msg.created_at).toLocaleString()}</p>
+          </span>)
+        }
+        return (
+          <span className="message-item-touser" key={index}>
+            <h5>{msg.first_name} {msg.last_name}:</h5>
+            <p>{msg.content}</p>
+            <p className="text-muted">{new Date(msg.created_at).toLocaleString()}</p>
+          </span>
+        )
+      }
+      return (<div key={index}></div>)
+    })
 
     return (
       <div className="message-container">
         <div className="message-block">
-          <h4>Messages</h4>
           <ul className="message-list">
-            {this.state.data.map((msg, index) =>
-              <div key={index}>
-              { msg.msg_dir === "fromuser" ?
-              <span className="message-item-fromuser">
-                <h5>Me:</h5>
-                <p>{msg.content}</p>
-                <p className="text-muted">{new Date(msg.created_at).toLocaleString()}</p>
-              </span>  
-              :
-              <span className="message-item-touser">
-                <h5>{msg.first_name} {msg.last_name}:</h5>
-                <p>{msg.content}</p>
-                <p className="text-muted">{new Date(msg.created_at).toLocaleString()}</p>
-              </span>  
-              } 
-              </div>
-            )}
+            {messages}
           </ul>
+          <Form className="contact" onSubmit={this.props.onSubmit}>
+            <Form.Group controlId="formBasicMessage">
+              {/* <Form.Label>Send message to {this.props.match.params.name}:</Form.Label> */}
+              <Form.Control as="textarea" placeholder="Enter message" />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="myBtn" block>
+              Send
+            </Button>
+          </Form>
         </div>
       </div>
     )
