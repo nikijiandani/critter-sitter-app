9b725571b624a443fd8d48aa3f7fc2fc4a502470
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-28 21:29:11 -0400

    Make search/profilelist responsive

--- src/components/ProfileListProfile.js
+++ src/components/ProfileListProfile.js
@@ -10,43 +10,47 @@ function ProfileListProfile(props) {
       <div className="item-details">
         <img src={props.user.avatar} alt="avatar" className="avatar" />
         <div className="item-info">
-          <span>{myIndex}.</span>
-          <Link to={`/profile/${props.user.user_id}`}>{props.user.first_name} {props.user.last_name}</Link>
-          <div className="rating">
-            <div className="rating-stars">
-              <StarRatingComponent 
-                name="rate1" 
-                starCount={5}
-                value={parseInt(props.user.avg_rating)}
-              />
+          <div>
+            <div>
+              <span>{myIndex}.</span>
+              <Link to={`/profile/${props.user.user_id}`}>{props.user.first_name} {props.user.last_name}</Link>
             </div>
-            <div className="rating-number">
-              { props.user.total_ratings !== null ?
-              <p>({props.user.total_ratings} ratings)</p>
+            <div className="rating">
+              <div className="rating-stars">
+                <StarRatingComponent 
+                  name="rate1" 
+                  starCount={5}
+                  value={parseInt(props.user.avg_rating)}
+                />
+              </div>
+              <div className="rating-number">
+                { props.user.total_ratings !== null ?
+                <p>({props.user.total_ratings} ratings)</p>
+                :
+                <p>(No ratings)</p>
+                }                
+              </div>
+            </div>
+            <div className="location">
+              {props.user.city}, ON, {props.user.postal_code}
+            </div>
+          </div>
+          <div className="pet">
+            <div className="pet-text">
+              { props.user.role === 2 ?
+              <p>Hosts:</p>
               :
-              <p>(Not rated yet)</p>
-              }                
+              <p>Owns:</p>
+              }
+            </div>
+            <div className="pet-icon">
+                {props.user.sitter_pet_types.map((pet) => 
+                <div key={pet.pet_type_id}>
+                  {pet.icon}
+                </div>
+                )}
             </div>
           </div>
-          <div className="location">
-            {props.user.city}, ON, {props.user.postal_code}
-          </div>
-        </div>
-      </div>
-      <div className="pet">
-        <div className="pet-text">
-          { props.user.role === 2 ?
-          <p>Hosts:</p>
-          :
-          <p>Owns:</p>
-          }
-        </div>
-        <div className="pet-icon">
-            {props.user.sitter_pet_types.map((pet) => 
-            <div key={pet.pet_type_id}>
-              {pet.icon}
-            </div>
-            )}
         </div>
       </div>
     </li>
