b2374bae71c341b46797842cd4b021648b930895
Nikita Jiandani <niki.jiandani@gmail.com>
2019-09-24 17:19:14 -0400

    prettier + fix home page links

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom"; 
-import BackgroundSlider from 'react-background-slider';
-import './styles/home.css';
-import bird from './images/bird.jpg';
-import cat from './images/cat.jpg';
-import cat1 from './images/cat1.jpg';
-import dog from './images/dog.jpg';
-import hedgehog from './images/hedgehog.jpg';
-import rabbit from './images/rabbit.jpg';
-import dograbbit from './images/dog-rabbit.jpg';
-import hedgehog2 from './images/hedgehog2.jpg';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import BackgroundSlider from "react-background-slider";
+import "./styles/home.css";
+import bird from "./images/bird.jpg";
+import cat from "./images/cat.jpg";
+import cat1 from "./images/cat1.jpg";
+import dog from "./images/dog.jpg";
+import hedgehog from "./images/hedgehog.jpg";
+import rabbit from "./images/rabbit.jpg";
+import dograbbit from "./images/dog-rabbit.jpg";
+import hedgehog2 from "./images/hedgehog2.jpg";
 
 class Home extends Component {
   render() {
@@ -17,9 +17,20 @@ class Home extends Component {
       <div className="home-container">
         <div className="overlay"></div>
         <div className="background-images">
-          <BackgroundSlider images={[ bird, dograbbit, cat, hedgehog2, dog, rabbit, cat1, hedgehog ]}
-                            duration={8}
-                            transition={0} />
+          <BackgroundSlider
+            images={[
+              bird,
+              dograbbit,
+              cat,
+              hedgehog2,
+              dog,
+              rabbit,
+              cat1,
+              hedgehog
+            ]}
+            duration={8}
+            transition={0}
+          />
         </div>
         <div className="text-section">
           <h1>Critter Sitter</h1>
@@ -27,20 +38,35 @@ class Home extends Component {
           <hr />
         </div>
         <div className="options-section">
-          {localStorage.getItem('loggedInUsersId') ?
-            parseInt(localStorage.getItem('loggedInUsersId')) === 1 ? 
-            <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn" >Show me Critter Sitters</Link> 
-            : 
-            <Link to="/search/customer" className="btn btn-lg btn-info homeBtn">Show me Critter Owners</Link>
-           : 
-          <>
-            <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn" >I need a Critter Sitter</Link>
-            <Link to="/search/customer" className="btn btn-lg btn-info homeBtn">I am a Critter Sitter</Link>
-          </>
-        }
+          {localStorage.getItem("loggedInUsersId") ? (
+            parseInt(localStorage.getItem("loggedInUsersId")) === 1 ? (
+              <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn">
+                Show me Critter Sitters
+              </Link>
+            ) : (
+              <Link
+                to="/search/customer"
+                className="btn btn-lg btn-info homeBtn"
+              >
+                Show me Critter Owners
+              </Link>
+            )
+          ) : (
+            <>
+              <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn">
+                I need a Sitter
+              </Link>
+              <Link
+                to="/search/customer"
+                className="btn btn-lg btn-info homeBtn"
+              >
+                I am a Sitter
+              </Link>
+            </>
+          )}
         </div>
       </div>
-    )
+    );
   }
 }
 
