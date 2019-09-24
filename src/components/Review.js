1eae8ab56ec8275eb7922766e34564367e237c4d
Nikita Jiandani <niki.jiandani@gmail.com>
2019-09-24 17:15:40 -0400

    Convert review author names+avatar's to links

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,30 +1,46 @@
-import React, { Component } from 'react';
-import StarRatingComponent from 'react-star-rating-component';
+import React, { Component } from "react";
+import StarRatingComponent from "react-star-rating-component";
 
 class Review extends Component {
-  render(){
+  render() {
     return (
       <div className="review">
         <div className="review-info">
           <StarRatingComponent
-            name="rating" 
+            name="rating"
             starCount={5}
             value={this.props.review.rating}
           />
           <p>
-            {new Date(this.props.review.created_at).toLocaleString(
-              'en-US', 
-              { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-            )}
+            {new Date(this.props.review.created_at).toLocaleString("en-US", {
+              weekday: "long",
+              year: "numeric",
+              month: "long",
+              day: "numeric"
+            })}
           </p>
         </div>
         <p>{this.props.review.content}</p>
         <div className="review-author">
-          <img src={this.props.review.avatar} alt="avatar" className="review-avatar" />
-          <strong>{this.props.review.first_name} {this.props.review.last_name}</strong>
+          <a
+            href={`http://localhost:3000/profile/${this.props.review.from_id}`}
+          >
+            <img
+              src={this.props.review.avatar}
+              alt="avatar"
+              className="review-avatar"
+            />
+          </a>
+          <a
+            href={`http://localhost:3000/profile/${this.props.review.from_id}`}
+          >
+            <strong>
+              {this.props.review.first_name} {this.props.review.last_name}
+            </strong>
+          </a>
         </div>
       </div>
-    )
+    );
   }
 }
 
