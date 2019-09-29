55259c378f269af7d776429a01b05230989616af
Nikita Jiandani <niki.jiandani@gmail.com>
2019-09-29 15:19:49 -0400

    Home component refactor

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import BackgroundSlider from "react-background-slider";
 import "./styles/home.css";
@@ -11,63 +11,53 @@ import rabbit from "./images/rabbit.jpg";
 import dograbbit from "./images/dog-rabbit.jpg";
 import hedgehog2 from "./images/hedgehog2.jpg";
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="home-container">
-        <div className="overlay"></div>
-        <div className="background-images">
-          <BackgroundSlider
-            images={[
-              bird,
-              dograbbit,
-              cat,
-              hedgehog2,
-              dog,
-              rabbit,
-              cat1,
-              hedgehog
-            ]}
-            duration={8}
-            transition={0}
-          />
-        </div>
-        <div className="text-section">
-          <h1>Critter Sitter</h1>
-          <h3>Search pet sitters near you!</h3>
-          <hr />
-        </div>
-        <div className="options-section">
-          {localStorage.getItem("loggedInUsersId") ? (
-            parseInt(localStorage.getItem("loggedInUsersId")) === 1 ? (
-              <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn">
-                Show me Critter Sitters
-              </Link>
-            ) : (
-              <Link
-                to="/search/customer"
-                className="btn btn-lg btn-info homeBtn"
-              >
-                Show me Critter Owners
-              </Link>
-            )
-          ) : (
-            <>
-              <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn">
-                I need a Sitter
-              </Link>
-              <Link
-                to="/search/customer"
-                className="btn btn-lg btn-info homeBtn"
-              >
-                I am a Sitter
-              </Link>
-            </>
-          )}
-        </div>
+export default function Home() {
+  return (
+    <div className="home-container">
+      <div className="overlay"></div>
+      <div className="background-images">
+        <BackgroundSlider
+          images={[
+            bird,
+            dograbbit,
+            cat,
+            hedgehog2,
+            dog,
+            rabbit,
+            cat1,
+            hedgehog
+          ]}
+          duration={8}
+          transition={0}
+        />
       </div>
-    );
-  }
+      <div className="text-section">
+        <h1>Critter Sitter</h1>
+        <h3>Search pet sitters near you!</h3>
+        <hr />
+      </div>
+      <div className="options-section">
+        {localStorage.getItem("loggedInUsersId") ? (
+          parseInt(localStorage.getItem("loggedInUsersId")) === 1 ? (
+            <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn">
+              Show me Critter Sitters
+            </Link>
+          ) : (
+            <Link to="/search/customer" className="btn btn-lg btn-info homeBtn">
+              Show me Critter Owners
+            </Link>
+          )
+        ) : (
+          <>
+            <Link to="/search/sitter" className="btn btn-lg btn-info homeBtn">
+              I need a Sitter
+            </Link>
+            <Link to="/search/customer" className="btn btn-lg btn-info homeBtn">
+              I am a Sitter
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
-
-export default Home;
