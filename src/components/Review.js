ca3a4eba04feabb69522ed3014de3d6484f31674
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-01 15:54:06 -0400

    Refactor Review component + prettier

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,47 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import StarRatingComponent from "react-star-rating-component";
 
-class Review extends Component {
-  render() {
-    return (
-      <div className="review">
-        <div className="review-info">
-          <StarRatingComponent
-            name="rating"
-            starCount={5}
-            value={this.props.review.rating}
-          />
-          <p>
-            {new Date(this.props.review.created_at).toLocaleString("en-US", {
-              weekday: "long",
-              year: "numeric",
-              month: "long",
-              day: "numeric"
-            })}
-          </p>
-        </div>
-        <p>{this.props.review.content}</p>
-        <div className="review-author">
-          <a
-            href={`http://localhost:3000/profile/${this.props.review.from_id}`}
-          >
-            <img
-              src={this.props.review.avatar}
-              alt="avatar"
-              className="review-avatar"
-            />
-          </a>
-          <a
-            href={`http://localhost:3000/profile/${this.props.review.from_id}`}
-          >
-            <strong>
-              {this.props.review.first_name} {this.props.review.last_name}
-            </strong>
-          </a>
-        </div>
+export default function Review({ review }) {
+  return (
+    <div className="review">
+      <div className="review-info">
+        <StarRatingComponent
+          name="rating"
+          starCount={5}
+          value={review.rating}
+        />
+        <p>
+          {new Date(review.created_at).toLocaleString("en-US", {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+          })}
+        </p>
       </div>
-    );
-  }
+      <p>{review.content}</p>
+      <div className="review-author">
+        <a href={`http://localhost:3000/profile/${review.from_id}`}>
+          <img src={review.avatar} alt="avatar" className="review-avatar" />
+        </a>
+        <a href={`http://localhost:3000/profile/${review.from_id}`}>
+          <strong>
+            {review.first_name} {review.last_name}
+          </strong>
+        </a>
+      </div>
+    </div>
+  );
 }
-
-export default Review;
