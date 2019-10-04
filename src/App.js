86fef4c84ce3396a2d8ee16a86cdd4718749756b
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-03 22:37:39 -0400

    App component refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles.css";
 import Mynavbar from "./Mynavbar.js";
@@ -11,68 +11,50 @@ import Contact from "./components/Contact.js";
 import Messages from "./components/Messages.js";
 import Error from "./components/Error.js";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentLoggedInUser: null
-    };
-  }
+export default function App() {
+  const [currentLoggedInUser, setCurrentLoggedInUser] = useState(null);
 
-  handleLogout = e => {
+  const handleLogout = e => {
     e.preventDefault();
     localStorage.clear();
-    this.setState({
-      currentLoggedInUser: null
-    });
+    setCurrentLoggedInUser(null);
     window.location.href = "http://localhost:3000/";
   };
 
-  handleLogin = e => {
+  const handleLogin = e => {
     e.preventDefault();
     localStorage.setItem("loggedInUsersEmail", e.target.elements[0].value);
     localStorage.setItem("loggedInUsersId", e.target.elements[1].value);
-    this.setState({
-      currentLoggedInUser: {
-        email: e.target.elements[0].value,
-        id: e.target.elements[1].value
-      }
+    setCurrentLoggedInUser({
+      email: e.target.elements[0].value,
+      id: e.target.elements[1].value
     });
   };
 
-  render() {
-    return (
-      <Router>
-        <Mynavbar
-          handleLogout={this.handleLogout}
-          currentUser={this.state.currentLoggedInUser}
+  return (
+    <Router>
+      <Mynavbar handleLogout={handleLogout} currentUser={currentLoggedInUser} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/search/:role" component={Search} />
+        <Route exact path="/profile/:id" component={Profile} />
+        <Route path="/profile/:id/contact/:name" component={Contact} />
+        <Route path="/signup" component={Signup} />
+        <Route
+          path="/login"
+          render={routeProps => (
+            <Login
+              handleLogin={handleLogin}
+              currentUser={currentLoggedInUser}
+            />
+          )}
         />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/search/:role" component={Search} />
-          <Route exact path="/profile/:id" component={Profile} />
-          <Route path="/profile/:id/contact/:name" component={Contact} />
-          <Route path="/signup" component={Signup} />
-          <Route
-            path="/login"
-            render={routeProps => (
-              <Login
-                handleLogin={this.handleLogin}
-                currentUser={this.state.currentLoggedInUser}
-              />
-            )}
-          />
-          <Route
-            path="/messages"
-            render={routeProps => (
-              <Messages currentUser={this.state.currentLoggedInUser} />
-            )}
-          />
-          <Route component={Error} />
-        </Switch>
-      </Router>
-    );
-  }
+        <Route
+          path="/messages"
+          render={routeProps => <Messages currentUser={currentLoggedInUser} />}
+        />
+        <Route component={Error} />
+      </Switch>
+    </Router>
+  );
 }
-
-export default App;
