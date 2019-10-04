b4489c1c5e73172aa0eed0efdedb652c52f8d82e
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-04 14:40:35 -0400

    Refactor Reviews component + prettier

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import './styles/profile.css';
-import './styles/reviews.css';
-import Review from './Review'
+import React from "react";
+import "./styles/profile.css";
+import "./styles/reviews.css";
+import Review from "./Review";
 
-class Reviews extends Component {
-  render(){
-    const reviews = this.props.reviews.map((review, index) => {
-      return <Review key={index} review={review}/>
-    })
-    return (
-      <div className="profile-review">
-          <h4>Reviews</h4>
-          <div className="reviews">
-              {reviews}
-          </div>
+export default function Reviews({ reviews }) {
+  return (
+    <div className="profile-review">
+      <h4>Reviews</h4>
+      <div className="reviews">
+        {reviews.map((review, index) => {
+          return <Review key={index} review={review} />;
+        })}
       </div>
-    )
-  }
+    </div>
+  );
 }
-
-export default Reviews;
